d362a7f93683d9c779c49ef952b14722521decdf
encode42 <me@encode42.dev>
2025-09-03 21:41:28 -0400

    Guest account cleanup POC

--- hosts/prospect/users/guest.nix
+++ hosts/prospect/users/guest.nix
@@ -17,6 +17,8 @@
     isNormalUser = true;
 
     description = "Guest";
+
+    password = "";
   };
 
   home-manager.users.guest = {
@@ -26,5 +28,27 @@
     ];
 
     home.stateVersion = "25.05";
+  };
+
+  systemd.services.clean-guest-home = {
+    description = "Perform guest account cleanup";
+    wantedBy = [ "halt.target" "reboot.target" ];
+    before = [ "halt.target" "reboot.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+
+      ExecStart = pkgs.writeShellScript "clean-guest-home" ''
+        find ${home-manager.users.guest.homeDirectory} \
+          -mindepth 1 \
+          -not -path "$HOME/.nix-profile" \
+          -not -path "$HOME/.nix-profile/*" \
+          -not -path "$HOME/.config" \
+          -not -path "$HOME/.config/*" \
+          -not -path "$HOME/.local" \
+          -not -path "$HOME/.local/*" \
+          -exec echo {} +
+      '';
+    };
   };
 }
