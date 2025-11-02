8365395f7f33bfe4d0b00ebd9d7a06141dc80ace
encode42 <me@encode42.dev>
2025-11-02 00:50:16 -0400

    Use GPG signing for git

--- homes/encode42/common/git.nix
+++ homes/encode42/common/git.nix
@@ -5,6 +5,13 @@
     userName = "encode42";
     userEmail = "me@encode42.dev";
 
+    signing = {
+      signByDefault = true;
+
+      format = "openpgp";
+      key = "2DB55D48E857C8C7C6DF239580B9576520BBBF2D";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
