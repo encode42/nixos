5054f06ba6783108eff4cde1786999247572dfd2
encode42 <me@encode42.dev>
2025-12-01 01:11:58 -0500

    Configure my SSH key for index

--- hosts/index/users/encode42.nix
+++ hosts/index/users/encode42.nix
@@ -16,11 +16,17 @@
     home.stateVersion = "25.05";
   };
 
-  users.users.encode42.extraGroups = [
-    "wheel"
-    "cdrom"
-    "optical"
+  users.users.encode42 = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM0jCVfIbVcgHOoDC8aUHDwUJu2mEqzvnqz3+9IeLhBO"
+    ];
 
-    "media"
-  ];
+    extraGroups = [
+      "wheel"
+      "cdrom"
+      "optical"
+
+      "media"
+    ];
+  };
 }
