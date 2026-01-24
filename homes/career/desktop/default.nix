0e14bf32dbce2e61243bd5e08e5eef07fb0581b8
encode42 <me@encode42.dev>
2026-01-24 00:40:03 -0500

    Move Modrinth App to a shared package

--- homes/career/desktop/default.nix
+++ homes/career/desktop/default.nix
@@ -8,6 +8,7 @@
 
     (import ./firefox.nix { inherit homeDesktop; })
 
+    (flakeRoot + /homes/shared/desktop/modrinth.nix)
     (flakeRoot + /homes/shared/desktop/prismlauncher.nix)
 
     ./packages.nix
--- homes/career/desktop/packages.nix
+++ homes/career/desktop/packages.nix
@@ -4,8 +4,5 @@
   home.packages = with pkgs; [
     slack
     notion-app-enhanced
-
-    # Awaiting the merge of #466573
-    #modrinth-app
   ];
 }
